d079676d7fce98344e245658dad3681f86d139e6
cschraut <152220458+cschraut@users.noreply.github.com>
2024-12-03 14:10:05 -0500

    Add files via upload

---
+++ Assets/EnemyMover.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FollowPath());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FollowPath(){
+        foreach(Waypoint waypoint in path){
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercentage = 0;
+            
+            transform.LookAt(endPosition);
+
+            while(travelPercentage < 1f){
+                travelPercentage = travelPercentage + Time.deltaTime/2;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercentage); 
+                yield return new WaitForEndOfFrame();
+            }
+        }
+    }
+}
